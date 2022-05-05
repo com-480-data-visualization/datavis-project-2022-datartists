4b8362fc773dca9806d01b6b735dd9bdaf066292
Marc Odermatt <marcoder@ethz.ch>
2022-05-05 20:44:16 +0200

    Working movies and titles, no color yet.

--- treemap.js
+++ treemap.js
@@ -1,4 +1,6 @@
 let genres;
+var actor_movies;
+var all_movies;
 
 function whenDocumentLoaded(action) {
     if (document.readyState === "loading") {
@@ -11,13 +13,15 @@ function whenDocumentLoaded(action) {
 
 whenDocumentLoaded(() => {
 
-    const actor_name = "Adam Adele"
+    const actor_name = "Tom Hanks"
     let charactersViz
     d3.json("/data/data.json").then(function (json) {
         console.log(json)
         const actor = json.actors.find(actor => actor.name === actor_name)
         console.log(actor)
-        const movies = json.movies.filter(movie => movie.id in actor.movies)
+        all_movies = json.movies
+        const movies = json.movies.filter(movie => actor.movies.includes(movie.id))
+        actor_movies = actor.movies
         console.log(movies)
         genres = json.genres
 
@@ -33,11 +37,19 @@ const font_config = {
     }
 };
 
+function genreFromMovie(movie){
+    if (movie.genres){
+        return genres.find(g => g.id === movie.genres[0])
+    } else{
+        return genres[0]
+    }
+}
+
 function visualizeMoviesTreeMap(movies) {
     return new d3plus.Treemap()
         .select('#treemap')
         .data(movies)
         .label(movie => movie.title)
-        //.color(movie => genres.find(function(genre){return genre.id === movie.genres[0]}).color)
+        //.color(movie => genreFromMovie(movie).color)
         .sum("budget")
 }
