c343eee0d91e962c7c96630a853e3873b17f870d
Cezary Januszek <cezary.januszek@epfl.ch>
2022-05-06 12:12:06 +0200

    Tom Cruise top 10 graph

---
+++ actor_network/connection_network.js
@@ -0,0 +1,34 @@
+
+function whenDocumentLoaded(action) {
+	if (document.readyState === "loading") {
+		document.addEventListener("DOMContentLoaded", action);
+	} else {
+		// `DOMContentLoaded` already fired
+		action();
+	}
+}
+
+  /*
+  * Example graph for 10 most connected actors to Tom Cruise and their interconnections
+  */
+
+  var nodes = []
+  d3.json("one_actor_nodes.json", function(data) {
+    for(let i = 0; i < data.length; ++i){
+      nodes.push(data[i])
+    }
+  });
+  console.log(nodes)
+
+  var edges = []
+  d3.json("one_actor_edges.json", function(data) {
+    for(let i = 0; i < data.length; ++i){
+      edges.push(data[i])
+    } 
+  });
+  console.log(edges)
+
+whenDocumentLoaded(() => { 
+  
+  let graph = new d3plus.Network().select('#network').links(edges).nodes(nodes).render();
+});
